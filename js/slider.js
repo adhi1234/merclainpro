d279fa02baac378a28e475ac5cfa976c79b3048a
Adarsh Sreekumar <adhi1234@users.noreply.github.com>
2018-01-01 21:48:27 +0530

    js_patch-created
    Java sctrpt added for animation--patch_updated

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+var nsOptions = { ...
+    thumbWidth: "50%",//As the thumbHeight is set to "auto", this 50% is the percentage
+    //of the gallery viewport width.
+    thumbHeight: "auto",
+    showMode: 3, //Then the active thumbnail always stays in the center of the gallery
+    ...
+}
